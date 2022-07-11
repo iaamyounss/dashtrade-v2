a0f845a9c8196768c18571f8c2ff1c6e0c03159e
iamyounss <contact@iamruby.com>
2022-07-11 08:40:32 +0200

    layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Layout from '../sections/Layout';
+import Layout from '../components/layout/Layout';
 
 const Home: NextPage = () => {
   return (
